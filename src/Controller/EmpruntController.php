ce8acef099162383437c2d626248b7cd4768ad11
pierickj <pierick.jacquet@gmail>
2026-03-10 16:05:07 +0100

    working on return book fct

--- src/Controller/EmpruntController.php
+++ src/Controller/EmpruntController.php
@@ -19,6 +19,7 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/emprunt')]
 final class EmpruntController extends AbstractController
 {
+// chemin qui montre tous les emprunts en cours
     #[Route(name: 'app_emprunt_index', methods: ['GET'])]
     public function index(EmpruntRepository $empruntRepository): Response
     {
@@ -26,13 +27,13 @@ final class EmpruntController extends AbstractController
             'emprunts' => $empruntRepository->findAll(),
         ]);
     }
-
+// Chemin pour faire un nouvel emprunt à l'appuie du bouton FONCTIONNE
     #[Route('/{id}/new', name: 'app_emprunt_new', methods: ['GET', 'POST'])]
     public function new($id, Request $request, BookRepository $bookRepository, EntityManagerInterface $entityManager, Book $book): Response
     {
         $emprunt = new Emprunt();
         $emprunt-> setBook($book);
-        $emprunt->setUser($this->getUser());
+        $emprunt-> setUser($this->getUser());
         $book = $bookRepository -> find($id);
 
         $emprunt->setDateEmprunt(new DateTimeImmutable());
@@ -42,55 +43,67 @@ final class EmpruntController extends AbstractController
 
         $entityManager->persist($emprunt);
         $entityManager->flush();
-        // setuser et set book pas forcément nécessaire
-        // $emprunt->setDateRetour(new DateTimeImmutable());
-        // $emprunt->setStatut("rendu");
-        // $emprunt->setUser($id);
-        // $emprunt->setBook($id);
-        // $currentStock = $book->getStock() + 1;
-        // $book->setStock($currentStock);
-
-        // $entityManager->persist($emprunt);
 
         $this->addFlash('success', "Livre emprunté.");
 
-        return $this->redirectToRoute('app_catalog', [], Response::HTTP_SEE_OTHER);
-        
-        
+        return $this->redirectToRoute('app_emprunt_index', [], Response::HTTP_SEE_OTHER);
     }
-
-        #[Route('/{id}/returnBook', name: 'app_emprunt_return', methods: ['GET', 'POST'])]
-    public function returnBook($id, Request $request, BookRepository $bookRepository, EntityManagerInterface $entityManager, Book $book): Response
+// Chemin pour faire un retour d'un livre à l'appuie du bouton restituer
+    #[Route('/{id}/returnBook', name: 'app_emprunt_return', methods: ['GET', 'POST'])]
+    public function returnBook($id, Request $request, BookRepository $bookRepository, Emprunt $emprunt, EntityManagerInterface $entityManager, Book $book): Response
     {
-        $emprunt = new Emprunt();
         $emprunt-> setBook($book);
         $emprunt->setUser($this->getUser());
         $book = $bookRepository -> find($id);
 
         $emprunt->setDateRetour(new DateTimeImmutable());
         $emprunt->setStatut("Restitué");
-        $currentStock = $book->getStock() + 1;
-        $book->setStock($currentStock);
 
-        $entityManager->persist($emprunt);
-        $entityManager->flush();
-        // setuser et set book pas forcément nécessaire
-        // $emprunt->setDateRetour(new DateTimeImmutable());
-        // $emprunt->setStatut("rendu");
-        // $emprunt->setUser($id);
-        // $emprunt->setBook($id);
+        $book = $emprunt->getBook();
+        $book->setStock($book->getStock() + 1);
         // $currentStock = $book->getStock() + 1;
         // $book->setStock($currentStock);
 
-        $entityManager->persist($emprunt);
+        // $entityManager->persist($emprunt);
+        $entityManager->flush();
 
-        $this->addFlash('success', "Livre emprunté.");
+        $this->addFlash('success', "Livre rendu.");
 
-
-        return $this->redirectToRoute('app_catalog', [], Response::HTTP_SEE_OTHER);
+        // retourne sur la liste des emprunts en cours
+        return $this->redirectToRoute('app_emprunt_index', [], Response::HTTP_SEE_OTHER);
 
         
     }
+
+// Code Gemini
+// #[Route('/{id}/return', name: 'app_emprunt_return', methods: ['GET', 'POST'])]
+// public function returnBook(
+//     Emprunt $emprunt, // Symfony récupère automatiquement l'emprunt via l'ID
+//     EntityManagerInterface $entityManager
+// ): Response {
+//     // 1. On vérifie si le livre n'a pas déjà été rendu pour éviter les doublons de stock
+
+
+//     // 2. Mise à jour de l'emprunt (pas de "new Emprunt")
+//     $emprunt->setDateRetour(new \DateTimeImmutable());
+//     $emprunt->setStatut("Restitué");
+
+//     // 3. Mise à jour du stock du livre associé
+//     $book = $emprunt->getBook();
+//     $book->setStock($book->getStock() + 1);
+
+//     // 4. On enregistre les modifications
+//     // Pas besoin de persist($emprunt) car l'objet vient déjà de la BDD
+//     $entityManager->flush();
+
+//     $this->addFlash('success', "Le livre a bien été restitué.");
+
+//     return $this->redirectToRoute('app_emprunt_index', [], Response::HTTP_SEE_OTHER);
+// }
+
+
+
+
 
     #[Route('/{id}/emprunt/history', name: 'app_emprunt_show', methods: ['GET', 'POST"'])]
     public function show($id, BookRepository $bookRepository, EmpruntRepository $empruntRepo): Response
@@ -103,7 +116,7 @@ final class EmpruntController extends AbstractController
             'book'=> $book
         ]);
     }   
-    
+    // C'est la route qui montre l'historique de tous les emprunts
     #[Route('/emprunt/all/', name: 'app_emprunt_all', methods: ['GET', 'POST"'])]
     public function showAll(UserRepository $userRepository, EmpruntRepository $empruntRepo, BookRepository $bookRepository): Response
     {
